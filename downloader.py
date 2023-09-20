4f70156a15b7b702f37afc05bb887884f222ba81
dhruv-mehrotra <dhruv_m@cs.iitr.ac.in>
2023-09-21 00:44:22 +0530

    removed path

--- downloader.py
+++ downloader.py
@@ -17,7 +17,7 @@ options = webdriver.ChromeOptions()
 options.add_argument('--headless')
 options.add_argument('--no-sandbox')
 options.add_argument('--disable-dev-shm-usage')
-driver = webdriver.Chrome(service=service, options=options)
+driver = webdriver.Chrome(options=options)
 
 driver.get(website)
 
